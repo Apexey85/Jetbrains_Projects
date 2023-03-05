f577d4223a571e1994c94e4adf14ce2f0f348b60
Alexey Grinenko <alexey.grinenko@hotmail.com>
2023-03-05 05:25:10 +0300

    commit

--- Contacts/src/contacts/Types/ContactRecord.java
+++ Contacts/src/contacts/Types/ContactRecord.java
@@ -2,25 +2,65 @@ package contacts.Types;
 
 
 public class ContactRecord {
-    public String name;
-    public String surname;
-    public String phoneNumber;
+    private String name;
+    private String surname;
+    private String phoneNumber;
 
-    public ContactRecord() {
-        this.name = "";
-        this.surname = "";
-        this.phoneNumber = "";
+    private ContactRecord(String name, String surname, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
     }
 
-    public void setName(String name){
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setSurname(String surname){
+    public void setSurname(String surname) {
         this.surname = surname;
     }
 
-    public void setPhoneNumber(String phoneNumber){
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    public static class Builder {
+        private String name;
+        private String surname;
+        private String phoneNumber;
+
+        public Builder() {}
+
+        public Builder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname){
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber){
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public ContactRecord build(){
+            return new ContactRecord(name, surname, phoneNumber);
+        }
+    }
+
 }
